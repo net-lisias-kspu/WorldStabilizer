1eb6047084e2c24e3bde3ab9e85251b1ee62ccbc
Lisias <me@lisias.net>
2020-10-10 06:42:02 -0300

    Adding KSPe installment checks facilities

---
+++ Source/WorldStabilizer/Startup.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+namespace WorldStabilizer
+{
+    [KSPAddon(KSPAddon.Startup.Instantly, true)]
+    internal class Startup : MonoBehaviour
+	{
+        private void Start()
+        {
+            Log.force("Version {0}", Version.Text);
+
+            try
+            {
+                KSPe.Util.Installation.Check<Startup>(typeof(Version));
+            }
+            catch (KSPe.Util.InstallmentException e)
+            {
+                Log.error(e.ToShortMessage());
+                KSPe.Common.Dialogs.ShowStopperAlertBox.Show(e);
+            }
+        }
+	}
+}
