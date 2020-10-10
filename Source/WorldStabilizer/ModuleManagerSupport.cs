61244705521a650894f3e252c37def0d5d0eaa53
Lisias <me@lisias.net>
2020-10-10 06:45:39 -0300

    Moving the whole shebang into the `net.lisias.ksp` "vendor" hierarchy

---
+++ Source/WorldStabilizer/ModuleManagerSupport.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+
+namespace WorldStabilizer
+{
+	public static class ModuleManagerSupport
+	{
+		public static IEnumerable<string> ModuleManagerAddToModList()
+		{
+			string[] r = {typeof(ModuleManagerSupport).Namespace};
+			return r;
+		}
+	}
+}
